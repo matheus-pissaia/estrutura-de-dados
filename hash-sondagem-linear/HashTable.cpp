6a6c57bf95b25332b02fb8d92892b4499d2ab379
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-10-24 00:11:52 -0300

    chore: add documentation

--- hash-sondagem-linear/HashTable.cpp
+++ hash-sondagem-linear/HashTable.cpp
@@ -7,6 +7,11 @@ class HashTable
     private:
         std::vector<std::string> _vector;
 
+        /* 
+            Checks if the position in the vector is empty.
+            It returns `true` if the position contains either "-1" or "-2", indicating that it is empty.
+            Otherwise, it returns `false`.
+        */
         bool _isEmpty(size_t& pos)
         {
             return _vector[pos] == "-1" || _vector[pos] == "-2";
@@ -19,6 +24,15 @@ class HashTable
             std::fill(_vector.begin(), _vector.end(), "-1");
         }
 
+        /* 
+            Inserts an element into the hash table.
+
+            If the position is empty, then the element is inserted there.
+
+            If the position is not empty:
+            1. Returns if the element on the current position is the same that we are trying to insert.
+            2. Search an empty position to be inserted through the vector, using the linear probing strategy.
+         */
         void insert(std::string& el)
         {
             size_t pos = hash(el);
@@ -52,6 +66,15 @@ class HashTable
             }
         }
 
+        /* 
+            Removes an element from the hash table.
+
+            If the position is empty, then the function just returns.
+
+            If the position is not empty:
+            1. Removes the element on the current position if is the same that we passed.
+            2. Search the element to be removed through the vector, using the linear probing strategy.
+         */
         void remove(std::string& el)
         {
             size_t pos = hash(el);
@@ -85,6 +108,13 @@ class HashTable
             }
         }
 
+        /* 
+        
+            The hash function that takes a string as input and returns the hash value. 
+        
+            The hash value is calculated as the length of the string modulo the size 
+            of the hash table vector.
+        */
         size_t hash(std::string& el)
         {
             return el.length() % _vector.size();
